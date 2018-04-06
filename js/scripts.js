23dab5133d503673444c2d76c3bfec551dc3874a
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-06 10:48:36 -0700

    Add pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 //Business Logic
-
+function Pizza(name, size, topping) {
+  this.name = name;
+  this.size = size;
+  this.topping = topping;
+  this.cost = 0;
+}
 
 // User Interface Logic
 $(function() {
