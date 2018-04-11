b5de706b042f29e3061bb9f0253ad61b1d30e8dd
Isaac Shoman <isaac@>
2018-04-11 10:19:51 -0700

    Clean up code. remove unused comments and functions

--- js/scripts.js
+++ js/scripts.js
@@ -23,12 +23,6 @@ Pizza.prototype.pizzaSizeCost = function () {
     this.cost = this.cost + (this.toppings.length * 1.5)
 };
 
-// Pizza.prototype.pizzaToppingCost = function () {
-//   for (index = 0; index < this.toppings.length; index ++) {
-//     this.cost = this.cost + 2;
-//   }
-// };
-
 // User Interface Logic
 $(function() {
   $("#pizza").submit(function(event){
@@ -42,33 +36,13 @@ $(function() {
     })
     var pizza = new Pizza(inputtedName, inputtedSize, inputtedToppings);
     console.log(pizza);
-
-  //   $("#topping").each(function() {
-  //    var secondTopping = $(this).find("#topping").val();
-  //    console.log(secondTopping);
-  //   //  var newTopping = new Topping(secondTopping);
-  //    pizza.toppings.push(topping);
-  //  });
-
     pizza.pizzaSizeCost();
-    // pizza.pizzaToppingCost();
     var pizzaCost = pizza.cost;
     $("span#cost").text("$" + pizzaCost);
     $("span#nameOutput").text(inputtedName + ", ")
     $("#output").show();
     $(".clearFields").hide();
     resetFields();
-  });
-
-  $("#moreToppings").click(function(){
-    $("#pizzaToppings").append('<div id="topping">' +
-                                '<div class="clearFields">' +
-                                  '<div class="form-group">' +
-                                    '<label for="pizzaSize">Select Pizza Topping:</label>' +
-                                    '<input id="name" class="form-control" type="text" placeholder="Enter Topping">' +
-                                  '</div>' +
-                                '</dive>' +
-                              '</div>');
   });
 
   function resetFields() {
